3a2e27e98626fbb16b917712ee5485447b43fd2c
Stella <gowildyj@gmail.com>
2025-07-25 00:44:28 +0900

    init project

---
+++ frontend/vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Atelier",
+        short_name: "Atelier",
+        theme_color: "#ffd1dc",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+  base: process.env.NODE_ENV === "development" ? "/" : "./",
+});

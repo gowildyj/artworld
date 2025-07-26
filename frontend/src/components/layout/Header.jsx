0672cb055e5d4b57db34b42ff07ad447f48c22d6
Stella <gowildyj@gmail.com>
2025-07-26 23:06:09 +0900

    navbar

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,13 +1,16 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 // import headerImage from "@assets/192.png";
 
 export default function Header() {
   return (
     <header className="site-header">
-      <div className="header-overlay">
-        {/* <img src={headerImage} alt="Site Logo" className="site-logo" /> */}
-        <h1 className="site-title">Little Atelier</h1>
-      </div>
+      <Link to="/" className="header-link">
+        <div className="header-overlay">
+          {/* <img src={headerImage} alt="Site Logo" className="site-logo" /> */}
+          <h1 className="site-title">Little Atelier</h1>
+        </div>
+      </Link>
     </header>
   );
 }
--- frontend/src/components/layout/Navigation.jsx
+++ frontend/src/components/layout/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Container, Nav, Navbar, Offcanvas, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState, useRef, useLayoutEffect } from "react";
 
@@ -15,58 +15,43 @@ export default function Navigation() {
     // { to: "/admin", label: "Admin" },
   ];
 
-  // Left Brand Check Width
-  const brandRef = useRef(null);
-  const [brandWidth, setBrandWidth] = useState(0);
-
-  useLayoutEffect(() => {
-    if (brandRef.current) {
-      setBrandWidth(brandRef.current.offsetWidth);
-    }
-  }, []);
-
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary px-3">
-        <Container
-          fluid
-          className="justify-content-between align-items-center w-100 px-3"
-        >
-          {/* Left Brand */}
-          <Navbar.Brand
-            as={Link}
-            to="/"
-            className="d-flex align-items-center justify-content-lg-center justify-content-start px-2"
-            ref={brandRef}
-          >
-            Petit Atelier
-          </Navbar.Brand>
-          <div className="d-lg-none">
-            <Navbar.Toggle onClick={() => setShowOffcanvas(true)} />
-          </div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid className="w-100 px-3 justify-content-center">
+          <Row className="w-100 align-items-center">
+            {/* Left Brand */}
+            <Col xs="auto" lg={3}>
+              <Navbar.Brand as={Link} to="/">
+                Mon Petit Atelier
+              </Navbar.Brand>
+            </Col>
 
-          {/* Center Nav Menu */}
-          <div className="d-none d-lg-flex justify-content-center flex-grow-1">
-            <Nav>
-              {menuItems.map(({ to, label }) => (
-                <Nav.Link key={to} as={Link} to={to}>
-                  {label}
-                </Nav.Link>
-              ))}
-            </Nav>
-          </div>
-
-          {/* Right Space */}
-          <div
-            className="d-none d-lg-block"
-            style={{ width: `${brandWidth}px` }}
-          >
-            <i className="bi bi-instagram me-2"></i>
-            <i className="bi bi-twitter"></i>
-          </div>
+            {/* Center Nav Menu */}
+            <Col lg={6} className="d-none d-lg-flex justify-content-center">
+              <Nav>
+                {menuItems.map(({ to, label }) => (
+                  <Nav.Link key={to} as={Link} to={to} className="mx-2">
+                    {label}
+                  </Nav.Link>
+                ))}
+              </Nav>
+            </Col>
+            {/* Right Space */}
+            <Col xs lg={3} className="d-flex justify-content-end">
+              <div className="d-none d-lg-block">
+                {/* SNS Icons can be placed here */}
+              </div>
+              <Navbar.Toggle
+                className="d-lg-none"
+                onClick={() => setShowOffcanvas(true)}
+              />
+            </Col>
+          </Row>
         </Container>
       </Navbar>
 
+      {/* Offcanvas Menu for Mobile */}
       <Offcanvas
         show={showOffcanvas}
         onHide={() => setShowOffcanvas(false)}
@@ -83,6 +68,7 @@ export default function Navigation() {
                 as={Link}
                 to={to}
                 onClick={() => setShowOffcanvas(false)}
+                className="fs-2"
               >
                 {label}
               </Nav.Link>
